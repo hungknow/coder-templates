9fb7f192048481e25576904d3346421d53044672
hungknow <hungbuiknow@gmail.com>
2024-12-27 15:24:31 +0700

    UPdate

--- src/modules/hvim/run.sh
+++ src/modules/hvim/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
+# Create folder if not exists
+mkdir -p ~/.config ~/local 
+
 REPO_URL="https://github.com/hungknow/nvim-config.git"
 CLONE_PATH=~/.config/nvim
-
+echo "nvim hello"
 curl -L https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz -C ~/local --strip-components=1
 rm nvim-linux64.tar.gz
-
-mkdir -p ~/.config
 
 if [ -z "$(ls -A $CLONE_PATH)" ]; then
   echo "Cloning $REPO_URL to $CLONE_PATH..."
