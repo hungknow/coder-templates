4228c45dab983e5317231f1f66b58011fd54c8b4
hungknow <hungbuiknow@gmail.com>
2024-12-27 15:28:26 +0700

    UPdate

--- src/modules/awscli/run.sh
+++ src/modules/awscli/run.sh
@@ -7,9 +7,9 @@ if command -v apt >/dev/null; then
   # rm -rf /var/lib/apt/lists/*
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
-  mkdir -p ~/local/aws-cli
-  mkdir -p ~/local/bin
-  ./aws/install --bin-dir ~/local/bin --install-dir ~/local/aws-cli 
+  mkdir -p ~/.local/aws-cli
+  mkdir -p ~/.local/bin
+  ./aws/install --bin-dir ~/.local/bin --install-dir ~/.local/aws-cli 
   rm awscliv2.zip
 elif command -v apk >/dev/null; then
   apk add --no-cache aws-cli
@@ -19,4 +19,4 @@ else
 fi
 
 echo "Installed AWS-CLI v2"
-~/local/bin/aws --version
+~/.local/bin/aws --version
--- src/modules/hvim/run.sh
+++ src/modules/hvim/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 # Create folder if not exists
-mkdir -p ~/.config ~/local 
+mkdir -p ~/.config ~/.local 
 
 REPO_URL="https://github.com/hungknow/nvim-config.git"
 CLONE_PATH=~/.config/nvim
 echo "nvim hello"
 curl -L https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -o nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz -C ~/local --strip-components=1
+tar xzvf nvim-linux64.tar.gz -C ~/.local --strip-components=1
 rm nvim-linux64.tar.gz
 
 if [ -z "$(ls -A $CLONE_PATH)" ]; then
